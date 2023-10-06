f431d27ccad6cfc12fd53ca75765e636d9a4b2cd
Christian <ps5christian@hotmail.com>
2023-10-06 23:30:31 +0200

    Added the rawFish Data to the API

--- index.js
+++ index.js
@@ -16,7 +16,18 @@ const woodLogFilePath = path.join(
   "woodLogs.json"
 );
 
+//finding the rawFish json file in the project
+const rawFishFilePath = path.join(
+  __dirname,
+  "Assets",
+  "js",
+  "food",
+  "rawFish.json"
+);
+
 const woodLogData = JSON.parse(fs.readFileSync(woodLogFilePath, "utf8"));
+
+const rawFishData = JSON.parse(fs.readFileSync(rawFishFilePath, "utf8"));
 
 //when visiting localhost:8000/ this message displays
 app.get("/", (req, res) => {
@@ -26,6 +37,33 @@ app.get("/", (req, res) => {
 //when visiting localhost:8000/woodLogs the different woodlogs data shows
 app.get("/woodLogs", (req, res) => {
   res.json(woodLogData.woodLogs);
+});
+
+//when visiting localhost:8000/rawFish the different raw fish & their location data shows
+app.get("/rawFish", (req, res) => {
+  //This combines all the data & shows the location
+  const raw_FishData = {
+    Shallow_Shores: rawFishData.Shallow_Shores,
+    Shrapnel_River: rawFishData.Shrapnel_River,
+    Trench_of_Despair: rawFishData.Trench_of_Despair,
+    Lemvor_Pier: rawFishData.Lemvor_Pier,
+    Open_Waters: rawFishData.Open_Waters,
+    Barren_Ocean: rawFishData.Barren_Ocean,
+  };
+
+  res.json(raw_FishData);
+  //This combines the data but dosen't show the location
+  /*const combinedRawFishData = [
+    ...rawFishData.Shallow_Shores,
+    ...rawFishData.Shrapnel_River,
+    ...rawFishData.Trench_of_Despair,
+    ...rawFishData.Lemvor_Pier,
+    ...rawFishData.Open_Waters,
+    ...rawFishData.Barren_Ocean,
+  ];
+
+  res.json(combinedRawFishData);
+  */
 });
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
